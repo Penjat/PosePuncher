9f11636ed3689c2a831a7828ced4d594df29acb6
Spencer <37875712+Penjat@users.noreply.github.com>
2021-10-30 20:38:29 -0700

    Set up with camera.

---
+++ PosePuncher/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ContentViewModel()
+    init() {
+        viewModel.checkAuthorization()
+    }
+    
+    var body: some View {
+        PlayerContainerView(captureSession: viewModel.captureSession)
+            .clipShape(Circle())
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PosePuncher/PosePuncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosePuncherApp.swift
+//  PosePuncher
+//
+//  Created by Spencer Symington on 2021-10-30.
+//
+
+import SwiftUI
+
+@main
+struct PosePuncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
