27e4a84c558d2ab14d9c1426acb935840b9083a5
Spencer <37875712+Penjat@users.noreply.github.com>
2021-11-02 17:32:18 -0700

    Randomly appearing circles.

--- PosePuncher/GameplayScene.swift
+++ PosePuncher/GameplayScene.swift
@@ -23,28 +23,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scene?.addChild(player.playerBody)
         
         player.setUpSink(posePublisher: poseViewModel.pose.eraseToAnyPublisher())
+        let update = SKAction.run(
+        {
+            let shape = SKShapeNode(circleOfRadius: 30 )
+            shape.position = CGPoint(x: CGFloat.random(in: 30..<(self.scene?.frame.maxX ?? 300)-30), y:-50)
+            shape.fillColor = .blue
+    //        shape.lineWidth = 10
+            shape.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+            shape.physicsBody?.isDynamic = false
+            shape.physicsBody?.affectedByGravity = false
+            shape.name = "ball"
+            shape.physicsBody!.contactTestBitMask = 1
+            let action = SKAction.moveTo(y: (self.scene?.frame.maxY ?? 300) + 50, duration: 10.0)
+            shape.run(action)
+            self.addChild(shape)
+        })
         
-        let shape = SKShapeNode(circleOfRadius: 50 )
-        shape.position = CGPoint(x: 600, y:300)
-        shape.fillColor = .blue
-        shape.lineWidth = 10
-        shape.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        shape.physicsBody?.isDynamic = false
-        shape.physicsBody?.affectedByGravity = false
-        shape.name = "ball"
-        shape.physicsBody!.contactTestBitMask = 1
-        addChild(shape)
-        
-        let shape2 = SKShapeNode(circleOfRadius: 50 )
-        shape2.position = CGPoint(x: (scene?.frame.maxX ?? 0) - 300, y: 300)
-        shape2.fillColor = .green
-        shape2.lineWidth = 10
-        shape2.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        shape2.physicsBody?.isDynamic = false
-        shape2.physicsBody?.affectedByGravity = false
-        shape2.name = "ball2"
-        shape2.physicsBody!.contactTestBitMask = 1
-        addChild(shape2)        
+        let seq = SKAction.sequence([SKAction.wait(forDuration: 2),update])
+        let createLoop = SKAction.repeatForever(seq)
+        run(createLoop)
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
