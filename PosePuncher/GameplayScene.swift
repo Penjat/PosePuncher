caf20409d6d801c0018eda5b400652f20c80e5a9
Spencer <37875712+Penjat@users.noreply.github.com>
2021-11-16 05:42:41 -0800

    Eyse only (#3)
    * Changed to just head.
    
    * Trial position.
    
    * Improved graphics.

--- PosePuncher/GameplayScene.swift
+++ PosePuncher/GameplayScene.swift
@@ -10,7 +10,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var score = 0
     let scoreLabel = SKLabelNode(text: "0")
     
-    let player = PlayerController()
+    var player: RocketController?
     var rightHand: SKEmitterNode?
     var leftHand: SKShapeNode?
     
@@ -18,31 +18,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         
         physicsWorld.contactDelegate = self
+        let spaceBackground = SKEmitterNode(fileNamed: "SpaceBackground")
+        spaceBackground?.position = CGPoint(x: 700, y: 0.0)
+        scene?.addChild(spaceBackground!)
     }
     
     func setUp() {
         videoViewModel.checkAuthorization()
         videoViewModel.setupOutput(delgate: poseViewModel)
-        scene?.addChild(player.playerBody)
         
+        player = RocketController(particleTarget: self)
+        scene?.addChild(player!.playerBody)
+        
+        
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
         scoreLabel.fontSize = 65
         scoreLabel.fontColor = SKColor.green
         scoreLabel.position = CGPoint(x: 80, y: 80)
         scoreLabel.zRotation = 3.14159
         addChild(scoreLabel)
         
-        player.setUpSink(posePublisher: poseViewModel.pose.eraseToAnyPublisher())
+        player?.setUpSink(posePublisher: poseViewModel.pose.eraseToAnyPublisher())
         let update = SKAction.run(
         {
-            let shape = SKShapeNode(circleOfRadius: 25 )
+            let shape = SKShapeNode(path: Star(corners: 5, smoothness: 0.5).path(in: CGRect(origin: CGPoint.zero, size: CGSize(width: 25, height: 25))) )
             shape.position = CGPoint(x: CGFloat.random(in: 30..<(self.scene?.frame.maxX ?? 300)-30), y:-50)
-            shape.fillColor = .orange
+//            shape.fillColor = .orange
             shape.physicsBody = SKPhysicsBody(circleOfRadius: 25)
-            shape.physicsBody?.isDynamic = false
+            shape.physicsBody?.isDynamic = true
             shape.physicsBody?.affectedByGravity = false
             shape.name = "ball"
             shape.physicsBody!.contactTestBitMask = 1
-            let action = SKAction.moveTo(y: (self.scene?.frame.maxY ?? 300) + 50, duration: 10.0)
+            let moveAction = SKAction.moveTo(y: (self.scene?.frame.maxY ?? 300) + 50, duration: 10.0)
+            let rotateAction =
+            SKAction.repeatForever(SKAction.rotate(byAngle: 3.1, duration: 2))
+            let action = SKAction.group([rotateAction,moveAction])
             shape.run(action)
             self.addChild(shape)
         })
---
+++ PosePuncher/RocketController.swift
@@ -0,0 +1,77 @@
+import SpriteKit
+import Combine
+
+class RocketController {
+    let playerBody: SKNode
+    var bag = Set<AnyCancellable>()
+    
+    var rightEye: SKShapeNode?
+    var leftEye: SKShapeNode?
+    var head: SKShapeNode?
+    
+    init(particleTarget: SKNode) {
+        playerBody = SKNode()
+        
+        rightEye = SKShapeNode(circleOfRadius: 10 )
+
+//        rightEye?.fillColor = .blue
+        rightEye?.strokeColor = .white
+        rightEye?.lineWidth = 2
+//        rightEye?.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+//        rightEye?.physicsBody?.isDynamic = true
+        rightEye?.physicsBody?.affectedByGravity = false
+        rightEye?.name = "rightEye"
+//        rightEye?.physicsBody!.contactTestBitMask = 1
+
+        playerBody.addChild(rightEye!)
+        
+        leftEye = SKShapeNode(circleOfRadius: 10 )
+        
+//        leftEye?.fillColor = .blue
+        leftEye?.strokeColor = .white
+        leftEye?.lineWidth = 2
+//        leftEye?.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+//        leftEye?.physicsBody?.isDynamic = true
+        leftEye?.physicsBody?.affectedByGravity = false
+        leftEye?.name = "leftEye"
+//        leftEye?.physicsBody!.contactTestBitMask = 1
+        
+        playerBody.addChild(leftEye!)
+        
+        head = SKShapeNode(rect: CGRect(x: -50, y: -50, width: 100, height: 100) )
+        
+        head?.strokeColor = .white
+        head?.lineWidth = 2
+        head?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 100))
+        head?.physicsBody?.isDynamic = false
+        head?.physicsBody?.affectedByGravity = false
+        head?.name = "fist"
+        head?.physicsBody!.contactTestBitMask = 1
+        playerBody.addChild(head!)
+        
+        let trail = SKEmitterNode(fileNamed: "RocketTrail")!
+        head?.addChild(trail)
+        trail.position = CGPoint(x: 0, y: 50)
+        trail.targetNode = particleTarget
+        
+    }
+    
+    func setUpSink(posePublisher: AnyPublisher<Pose, Never>) {
+        posePublisher.sink { pose in
+            let joints = pose.joints.map{ $0.value }.filter{ $0.isValid}
+            joints.forEach { joint in
+                switch joint.name {
+                case .rightEye:
+                    self.rightEye?.position = joint.position
+                case .leftEye:
+                    self.leftEye?.position = joint.position
+                case .nose:
+                    self.head?.position = CGPoint(x: joint.position.x, y: joint.position.y)
+                default:
+                    break
+                }
+            }
+//            self.head?.position = CGPoint(x: (self.rightEye!.position.x + self.leftEye!.position.x)/2.0, y: (self.rightEye!.position.y + self.leftEye!.position.y)/2.0)
+        }.store(in: &bag)
+    }
+}
