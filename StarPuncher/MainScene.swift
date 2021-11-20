e2b02a8f55fb97c081bb39c2da807eb61cbd1dfc
Spencer <37875712+Penjat@users.noreply.github.com>
2021-11-20 07:31:45 -0800

    drawing more joints.

--- StarPuncher/MainScene.swift
+++ StarPuncher/MainScene.swift
@@ -1,33 +1,55 @@
 import SpriteKit
 
 class MainScene: SKScene, SKPhysicsContactDelegate {
-    var playerNose: SKShapeNode?
+    lazy var playerParts: [Joint.Name: SKShapeNode] = [.nose: nose,
+                                                  .rightEye: eye,
+                                                  .leftEye: eye,
+                                                       .rightShoulder: joint,
+                                                       .leftShoulder: joint,
+                                                       .rightElbow: joint,
+                                                       .leftElbow: joint,
+                                                       .rightWrist: joint,
+                                                       .leftWrist: joint]
     override func didMove(to view: SKView) {
-        
         physicsWorld.contactDelegate = self
         let spaceBackground = SKEmitterNode(fileNamed: "SpaceBackground")
         spaceBackground?.position = CGPoint(x: 700, y: 0.0)
         scene?.addChild(spaceBackground!)
         
-        playerNose = SKShapeNode(circleOfRadius: 10)
-        playerNose?.fillColor = .blue
-        if let nose = playerNose {
-            scene?.addChild(nose)
-        }
-        
-//
+        playerParts.values.forEach { self.scene?.addChild($0)}
+    }
+    
+    var nose: SKShapeNode {
+        let node = SKShapeNode(circleOfRadius: 5)
+        node.fillColor = .blue
+        return node
+    }
+    
+    var eye: SKShapeNode {
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .green
+        return node
+    }
+    
+    var joint: SKShapeNode {
+        let node = SKShapeNode(circleOfRadius: 5)
+        node.fillColor = .blue
+        return node
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         guard let nodeA = contact.bodyA.node else { return }
         guard let nodeB = contact.bodyB.node else { return }
-        
     }
     
     func drawPlayer(pose: Pose) {
         guard pose.confidence > 0.05 else {
             return
         }
-        playerNose?.position = pose.joints.first(where: { $0.key == .nose})?.value.position ?? CGPoint()
+        pose.joints.forEach({ (key: Joint.Name, value: Joint) in
+            if let node = playerParts[key] {
+                node.position = value.position
+            }
+        })
     }
 }
