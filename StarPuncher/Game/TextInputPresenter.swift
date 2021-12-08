104c3430990540f1e99c8069521432c1e863f292
Spencer <37875712+Penjat@users.noreply.github.com>
2021-12-08 12:28:15 -0800

    Fixed rainbow.

--- StarPuncher/Game/TextInputPresenter.swift
+++ StarPuncher/Game/TextInputPresenter.swift
@@ -8,22 +8,25 @@ class TextInputPresenter {
         let radius: CGFloat = 180.0
         let bodyNode = SKNode()
         aplhabet.uppercased().enumerated().forEach { (index, letter) in
-            let keyNode = letterNode(String(letter))
-            bodyNode.addChild(keyNode)
+            
             let theta: CGFloat = (CGFloat(index)/CGFloat(aplhabet.count)) * -2 * CGFloat.pi
+            
+            let red: CGFloat = (sin(-theta - (0 * 2*CGFloat.pi/3.0)) + 1)/2
+            let blue: CGFloat = (sin(-theta - ( 2*2*CGFloat.pi/3.0)) + 1)/2
+            let green: CGFloat = (sin(-theta - ( 1*2*CGFloat.pi/3.0)) + 1)/2
+            
+            var color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            
+            let keyNode = letterNode(String(letter), color: color)
             keyNode.position = CGPoint(x: cos(theta)*radius , y: sin(theta)*radius)
-            
-            let red: CGFloat = sin(-theta*3 - (0 * 2*CGFloat.pi/3.0)) + 0.5
-            let blue: CGFloat = sin(-theta*3 - ( 2*2*CGFloat.pi/3.0)) + 0.5
-            let green: CGFloat = sin(-theta*3 - ( 1*2*CGFloat.pi/3.0)) + 0.5
-            
-            keyNode.fontColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            bodyNode.addChild(keyNode)
         }
         
         return bodyNode
     }()
     
-    static func letterNode(_ letter: String) -> SKLabelNode {
+    static func letterNode(_ letter: String, color: UIColor) -> SKLabelNode {
+        let newColor = color.copy() as! UIColor
         let letterNode = SKLabelNode(text: letter)
         letterNode.zRotation = 3.14159
         let circle = SKShapeNode.init(circleOfRadius: 20)
@@ -35,11 +38,16 @@ class TextInputPresenter {
         circle.physicsBody!.categoryBitMask = 0x00000001
         circle.physicsBody!.contactTestBitMask = 0x00000002
         circle.physicsBody!.collisionBitMask = 0x00000002
-        
+        letterNode.fontColor = newColor
+        circle.lineWidth = 2
+        circle.strokeColor = newColor
         circle.zPosition = -5
         letterNode.fontName = "AvenirNext-Bold"
+        
         letterNode.addChild(circle)
-        circle.position = CGPoint(x: 0 ,y: 10)
+        circle.position = CGPoint(x: 0 ,y: 11)
+        
+        print("\(letterNode.fontColor) \(circle.strokeColor)")
         
         return letterNode
     }
