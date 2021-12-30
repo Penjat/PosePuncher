527bb885084fca4c324326bcd29e38152c7ee904
Spencer <37875712+Penjat@users.noreply.github.com>
2021-12-30 16:52:18 -0330

    Triggering keystrokes.

--- PosePuncher/ContentView.swift
+++ PosePuncher/ContentView.swift
@@ -1,22 +1,56 @@
 import SwiftUI
 import SpriteKit
+import Combine
 
 struct ContentView: View {
-    var scene: SKScene = {
-        let scene = GameScene()
-        scene.size = CGSize(width: 1280, height: 720)
-//        scene.scaleMode = .fill
-        scene.scaleMode = .aspectFit
-        scene.setUp()
-        return scene
-    }()
+    @State var cancellable: AnyCancellable?
+    @State var flag = false
+    let rightArrowKeyCode: UInt16 = 125
+    //    var scene: SKScene = {
+    //        let scene = GameScene()
+    //        scene.size = CGSize(width: 1280, height: 720)
+    ////        scene.scaleMode = .fill
+    //        scene.scaleMode = .aspectFit
+    //        scene.setUp()
+    //        return scene
+    //    }()
     
     var body: some View {
-        ZStack {
-            SpriteView(scene: scene)
-                .rotationEffect(Angle(degrees: 180))
-//                .frame(width: 1280, height: 720)
-                .ignoresSafeArea()
+        //        ZStack {
+        //            SpriteView(scene: scene)
+        //                .rotationEffect(Angle(degrees: 180))
+        ////                .frame(width: 1280, height: 720)
+        //                .ignoresSafeArea()
+        //        }
+        
+        Text("heelo").onAppear {
+            cancellable = Timer.publish(every: 2.0, on: .main, in: .default)
+                .autoconnect()
+                .receive(on: DispatchQueue.main).sink(receiveValue: { _ in
+                    print("ping")
+                    
+                    
+                    
+                    print("key down")
+                    let src = CGEventSource(stateID: .privateState)
+                    let keyDownEvent = CGEvent(keyboardEventSource: src, virtualKey: rightArrowKeyCode, keyDown: true)
+//                    keyDownEvent?.flags = CGEventFlags.maskCommand
+                    
+                    keyDownEvent?.setSource(src)
+                    keyDownEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+                    
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        print("key up")
+                        
+                        let keyUpEvent = CGEvent(keyboardEventSource: src, virtualKey: rightArrowKeyCode, keyDown: false)
+                    
+                        
+//                        keyUpEvent?.flags = CGEventFlags.maskCommand
+                        keyUpEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+                    }
+                })
+            //                    .assign(to: \.lastUpdated, on: myDataModel)
         }
     }
 }
