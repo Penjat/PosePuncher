825e1b14ac570c0b83c517d3be2b3bf5a0b93747
Spencer <37875712+Penjat@users.noreply.github.com>
2021-12-31 15:19:59 -0330

    More controls.

--- PosePuncher/ContentView.swift
+++ PosePuncher/ContentView.swift
@@ -6,8 +6,7 @@ struct ContentView: View {
     @StateObject var keyTracker = KeyTracker()
     @State var bag = Set<AnyCancellable>()
     @State var flag = false
-    let rightArrowKeyCode: UInt16 = 125
-    
+    let sensitivity = 40.0
     
     var scene: GameScene = {
         let scene = GameScene()
@@ -20,25 +19,60 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            SpriteView(scene: scene)
-                .rotationEffect(Angle(degrees: 180))
+//            z
             
             PlayerContainerView(captureSession: scene.videoViewModel.captureSession).onAppear {
                 scene.videoViewModel.checkAuthorization()
                 //                scene.videoViewModel.setupOutput(delgate: scene.poseViewModel)
                 scene.poseViewModel.pose.sink { pose in
                     let headHeight = pose.joints[.nose]?.position.y ?? 500
-                    if pose.joints[.rightWrist]?.position.y ?? 0 < headHeight {
+                    
+                    let headPosition = pose.joints[.nose]?.position ?? CGPoint.zero
+                    let center = CGPoint(x: scene.frame.midX ?? 0, y: scene.frame.midY ?? 0)
+                    if headPosition.x > center.x + sensitivity {
+                        keyTracker.keyOn(ButtonEvent(keyNUmber: 123))
+                        
+                    } else {
+                        keyTracker.keyOff(ButtonEvent(keyNUmber: 123))
+                    }
+                    
+                    if headPosition.x < center.x - sensitivity {
+                        keyTracker.keyOn(ButtonEvent(keyNUmber: 124))
+                        
+                    } else {
+                        keyTracker.keyOff(ButtonEvent(keyNUmber: 124))
+                    }
+                    
+                    if headPosition.y > center.y + sensitivity {
+                        keyTracker.keyOn(ButtonEvent(keyNUmber: 125))
+                        
+                    } else {
+                        keyTracker.keyOff(ButtonEvent(keyNUmber: 125))
+                    }
+                    
+                    if headPosition.y < center.y - sensitivity {
                         keyTracker.keyOn(ButtonEvent(keyNUmber: 126))
+                        
                     } else {
                         keyTracker.keyOff(ButtonEvent(keyNUmber: 126))
                     }
                     
-                    if pose.joints[.rightWrist]?.position.y ?? 0 > headHeight + 250 {
-                        keyTracker.keyOn(ButtonEvent(keyNUmber: 125))
+                    if pose.joints[.rightWrist]?.position.y ?? 0 < headHeight {
+                        keyTracker.keyOn(ButtonEvent(keyNUmber: 7))
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            keyTracker.keyOff(ButtonEvent(keyNUmber: 7))
+                        }
                     } else {
-                        keyTracker.keyOff(ButtonEvent(keyNUmber: 125))
+                        keyTracker.keyOff(ButtonEvent(keyNUmber: 7))
                     }
+//
+                    if pose.joints[.leftWrist]?.position.y ?? 0 > headHeight + 250 {
+                        keyTracker.keyOn(ButtonEvent(keyNUmber: 6))
+                    } else {
+                        keyTracker.keyOff(ButtonEvent(keyNUmber: 6))
+                    }
+                    
+                    
                 }.store(in: &bag)
                 
             }.opacity(0.4)
